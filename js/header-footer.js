51efdbaddb67a6f907e344e83b7f5f2a3c1c5abb
Silvina Riera <silvinariera83@gmail.com>
2023-06-02 20:58:44 -0300

    Update header-footer.js

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,6 +1,6 @@
 const headerTemplate = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container-fluid col-10">
-  <a class="navbar-brand" href="#">
+  <a class="navbar-brand" href="index.html">
     <img src="assets/images/codoacodo.png" alt="Codo a Codo" height="50">Conf Bs As</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
     aria-controls="navbarNav" aria-expanded="false" aria-label="Menu Principal">
@@ -9,7 +9,7 @@ const headerTemplate = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark"
   <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" aria-current="page" href="index.html">La conferencia</a>
+        <a class="nav-link" href="#">La conferencia</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#oradores">Los oradores</a>
@@ -21,7 +21,7 @@ const headerTemplate = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark"
         <a class="nav-link" href="#quieroSerOrador">Conviértete en orador</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link active" href="#">Comprar tickets</a>
+        <a class="nav-link active" aria-current="page" href="#">Comprar tickets</a>
       </li>
     </ul>
   </div>
