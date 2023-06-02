5812a5e1e8e17c467b16462e09193124964e4f21
Silvina Riera <silvinariera83@gmail.com>
2023-06-02 20:53:01 -0300

    Update header-footer.js

--- js/header-footer.js
+++ js/header-footer.js
@@ -9,7 +9,7 @@ const headerTemplate = `<nav class="navbar navbar-expand-lg navbar-dark bg-dark"
   <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" aria-current="page" href="#">La conferencia</a>
+        <a class="nav-link" aria-current="page" href="index.html">La conferencia</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#oradores">Los oradores</a>
